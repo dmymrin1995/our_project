baf1921dc2e66af09bfb15f90b80f3a654e3b369
Flambo <ddiimmaaq123@gmail.com>
2023-09-14 18:10:44 +0800

    Созданы хтмл, ксс, скрипт и запуск сервака

---
+++ Server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const hostname = '127.0.0.1'; 
+const port = 3000;
+http.createServer(function(request, response){
+      
+    console.log(`Запрошенный адрес: ${request.url}`);
+    // получаем путь после слеша
+    const filePath = request.url.substr(1);
+    // смотрим, есть ли такой файл
+    fs.access(filePath, fs.constants.R_OK, err => {
+        // если произошла ошибка - отправляем статусный код 404
+        if(err){
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }
+        else{
+            fs.createReadStream(filePath).pipe(response);
+        }
+      });
+}).listen(3000, function() {
+  console.log('Сервер запущен http://localhost:3000/index.html');
+});
